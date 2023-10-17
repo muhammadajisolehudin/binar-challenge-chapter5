ec4f99c9094ae700e1452f696f631b6bc2f8fb99
adindawww <adindawahyu680@gmail.com>
2023-10-17 22:57:41 +0700

    First commit

--- src/routes/RouteList.jsx
+++ src/routes/RouteList.jsx
@@ -3,12 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import ReactDOM  from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ThemeProvider } from '@material-tailwind/react'
-
 import { LoginPage } from '../pages/auth/LoginPage'
-import { RegisterPage } from '../pages/auth/RegisterPage'
-import { DashboardPage } from '../pages/movie/DashboradPage'
-import { DetailPage } from '../pages/movie/DetailPage'
 
 export const RouteList = () => {
   
@@ -21,14 +16,9 @@ export const RouteList = () => {
     <BrowserRouter>
       {/* menggunakan react query */}
       <QueryClientProvider client={queryMovie}>
-        <ThemeProvider>
         <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/movie/:id" element={<DetailPage />} />
         </Routes>
-        </ThemeProvider>
       </QueryClientProvider>
     </BrowserRouter>
   )
--- src/utils/http.jsx
+++ src/utils/http.jsx
@@ -1,27 +1,11 @@
 import axios from "axios";
-import { CookieKeys, CookieStorage } from "./cookies";
 
-// const Token = CookieStorage.get(CookieKeys.AuthToken)
-//pondasi axios
-const http3 = axios.create({
-    baseURL : process.env.REACT_APP_SERVER,
-    timeout : 30000,
-    headers : {
-        accept: 'application/json',
-        "Content-Type" : "application/json"
-    } 
-})
-
-http3.interceptors.request.use(
-    (config)=>{
-        config.headers={
-            ...config.headers,
-            Authorization: `Bearer ${CookieStorage.get(CookieKeys.AuthToken) ? CookieStorage.get(CookieKeys.AuthToken) : ""}`
-            // Authorization: `Bearer ${ Token ? Token : ""}`
-        }
-
-        return config
-    },
-)
-
-export {http3}
+const http = axios.create({
+  baseURL: process.env.REACT_APP_SERVER,
+  timeout: 30000,
+  headers: {
+    Accept: "application/json",
+    Authorization: `${process.env.REACT_APP_KEY}`,
+  },
+});
+export default http;
